32eee182420227123fda8e94c96111c491936a87
Dylan Collins <dcollins5590@students.mc3.edu>
2020-04-10 13:08:36 -0400

    added helmet

--- api/server.js
+++ api/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const server = express();
+const helmet = require('helmet')
 const projectsRouter = require('../Projects/projects-router');
 const actionsRouter = require('../actions/action-router');
 
 server.use(express.json());
+server.use(helmet())
 
 server.use('/api/projects', projectsRouter);
 server.use('/api/actions', actionsRouter)
